a3caa2c46766c10ef11fdc2a42159c98444f1a98
clip <binderclip2014@gmail.com>
2018-03-11 11:26:55 +0800

    add text_on_menubar

---
+++ plugins/text_on_menubar.1h.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env PYTHONIOENCODING=UTF-8 /usr/local/bin/python3
+# <bitbar.title>Text on Menubar</bitbar.title>
+# <bitbar.version>1.0</bitbar.version>
+# <bitbar.author>clip</bitbar.author>
+# <bitbar.author.github>binderclip</bitbar.author.github>
+# <bitbar.desc>Show your text on menubar</bitbar.desc>
+# <bitbar.dependencies>python3</bitbar.dependencies>
+import argparse
+import os
+from subprocess import call
+
+
+def get_text_file():
+    home = os.path.expanduser("~")
+    text_file = os.path.join(home, '.bitbar_text_on_menubar')
+    return text_file
+
+
+def get_file_path():
+    return os.path.realpath(__file__)
+
+
+def get_file_name():
+    return os.path.basename(__file__)
+
+
+def read_and_print():
+    text_file = get_text_file()
+    text = ''
+    try:
+        with open(text_file, 'r', encoding='UTF-8') as f:
+            text = f.read()
+    except FileNotFoundError:
+        pass
+    print(text.strip() or 'Hello')
+
+
+def set_text():
+    text = input('text to show: ')
+    text_file = get_text_file()
+    with open(text_file, 'w') as f:
+        f.write(text)
+    # refresh
+    s = "bitbar://refreshPlugin?name={}".format(get_file_name())
+    call(['open', s])
+
+
+def print_submenu():
+    print('---')
+    print('Set the Text | bash="{}" param1="-s"  terminal=true'.format(get_file_path()))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--set_text", action="store_true", help='set the text')
+    args = parser.parse_args()
+
+    if args.set_text:
+        set_text()
+        return
+    read_and_print()
+    print_submenu()
+
+
+if __name__ == '__main__':
+    main()
